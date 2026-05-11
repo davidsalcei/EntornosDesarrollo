8796d8e87ab7babb3ae9ffecd0fc5984675dda86
david.salcei <david.salcei@educa.jcyl.es>
2026-05-11 19:11:25 +0200

    Revision clase nueva

---
+++ src/ed_ejercicios51/Ordenador.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ed_ejercicios51;
+
+/**
+ *
+ * @author Clase
+ */
+public class Ordenador {
+    
+    private String motherboard;
+    private String cpu;
+    private int ram;
+
+    public Ordenador(String motherboardIn, String cpuIn, int ramIn) {
+        this.motherboard = motherboardIn;
+        this.cpu = cpuIn;
+        this.ram = ramIn;
+    }
+    
+}
